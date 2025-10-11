d3a3c9b86602519451d9fb730020c746d6791762
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-10-11 18:15:42 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/LOGO.svg" sizes="any" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

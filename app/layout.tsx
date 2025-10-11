b6ee5ffd55cc3d963f941b81b71bb8b8c9774a22
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-10-11 19:25:00 +0100

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SmoothScrollWrapper from "@/components/animation/SmoothScrollWrapper";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -50,6 +51,7 @@ export default function RootLayout({
       >
         <SmoothScrollWrapper />
         {children}
+        <Analytics />
       </body>
     </html>
   );

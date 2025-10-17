dd35a4e6c14efcf7dc3b0c7834f24df5d8844d17
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-10-17 19:37:09 +0100

    update animations images

--- components/ui/ScrollImages.tsx
+++ components/ui/ScrollImages.tsx
@@ -44,7 +44,7 @@ View all your existing connections, send new invitations, and explore studios or
 
 export function ScrollImages() {
   return (
-    <div className="h-[100lvh] flex items-center w-full overflow-hidden">
+    <div className="h-[100lvh] flex items-center w-full  ">
       <AnimatedTestimonials testimonials={images} />
     </div>
   );

7cae78f096ed365c243fd80fe414fa3bbb753b5d
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-10-18 21:45:13 +0100

    Update Footer.tsx

--- components/pages/home-page/footer/Footer.tsx
+++ components/pages/home-page/footer/Footer.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-import Image from "next/image";
 import Copyright from "@/components/ui/Copyright";
 import TermsC from "@/components/TermsC";
 import Privacy from "@/components/Privacy";
 import Contact from "@/components/Contact";
+import Logo from "@/components/ui/Logo";
 
 export default function Footer() {
   return (
     <footer className=" z-50 text-white  flex flex-col gap-10 items-center justify-center py-[66px] bg-[#232a3f] ">
-      <Image src={"/logo.svg"} alt="app logo" width={120} height={120} />
+      <Logo id="footer-logo" />
       <nav className="flex gap-10 px-3 text-sm text-center">
         <Privacy />
         <TermsC />

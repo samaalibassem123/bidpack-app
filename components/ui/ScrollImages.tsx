f6b6846d2cc277fd7e19c7f9ce6b6382726ad10b
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-10-18 20:51:30 +0100

    Update ScrollImages.tsx

--- components/ui/ScrollImages.tsx
+++ components/ui/ScrollImages.tsx
@@ -8,53 +8,39 @@ import {
 } from "@/components/ui/carousel";
 interface SlideData {
   title: string;
-  button: string;
   src: string;
   desc: string;
   text: string;
 }
 const images: SlideData[] = [
   {
-    text: `Build your own pool of trusted vendors.
-Add approved studios and service providers, review their profiles, and manage availability — ensuring you always have the right talent for every project.`,
-    title: "Vendor Pool",
-    desc: "where you can find vendors",
-    src: "/assets/VendorPool.jpeg",
-    button: "test",
-  },
-  {
-    text: `Keep track of every project in one place.
-Easily monitor progress, deadlines, and statuses of your ongoing and completed projects. Manage tasks efficiently and stay updated on every stage of production.`,
-    title: "Projects",
-    desc: "Check your projects ",
-    src: "/assets/MyProjects.jpeg",
-    button: "test",
-  },
-  {
     text: `Collaborate in real time with your partners.
 Our built-in messaging system lets you chat directly with studios and vendors, discuss project details, share updates, and keep communication organized and professional.`,
-    title: "Meesages",
+    title: "Collaborate in real time with your vendors",
     desc: "a messagary system where you can send and recieve messages with your team",
     src: "/assets/Messages.jpeg",
-    button: "test",
   },
   {
     text: `Stay in control of who joins your network.
 Review and approve incoming connection requests from studios and vendors interested in collaborating with you. Build trusted relationships with a single click.
         `,
-    title: "Connections",
+    title: "Stay in control of who joins your network",
     desc: "connections requests",
     src: "/assets/ConnectionReq.jpeg",
-    button: "test",
   },
   {
-    text: `Manage and grow your professional network effortlessly.
-View all your existing connections, send new invitations, and explore studios or vendors that match your project needs — all from one organized dashboard.
-        `,
-    title: "Connections",
-    desc: "see connections",
-    src: "/assets/MyConnections.jpeg",
-    button: "test",
+    text: `Build your own pool of trusted vendors.
+Add approved studios and service providers, review their profiles, and manage availability — ensuring you always have the right talent for every project.`,
+    title: "Create your Vendor Pool",
+    desc: "where you can find vendors",
+    src: "/assets/VendorPool.jpeg",
+  },
+  {
+    text: `Keep track of every project in one place.
+Easily monitor progress, deadlines, and statuses of your ongoing and completed projects. Manage tasks efficiently and stay updated on every stage of production.`,
+    title: "Create your projects and assign them to your prefered Vendors",
+    desc: "Check your projects ",
+    src: "/assets/MyProjects.jpeg",
   },
 ];
 
@@ -62,7 +48,7 @@ export function ScrollImages() {
   return (
     <div
       id="Features"
-      className=" h-[100lvh] relative flex items-center    w-full"
+      className=" h-[120lvh] relative flex items-center w-full"
     >
       <Carousel className="w-full">
         <CarouselContent>
@@ -73,8 +59,8 @@ export function ScrollImages() {
             >
               <img
                 src={img.src}
-                height={500}
-                width={500}
+                height={60}
+                width={630}
                 className=" rounded-3xl"
                 alt={img.title}
               />

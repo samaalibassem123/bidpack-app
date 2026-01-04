49080a397cbefe6ed1bf93999651dac4c0d2807c
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2026-01-04 21:51:10 +0100

    update teh get started

--- components/pages/home-page/hero-section/GetStarted.tsx
+++ components/pages/home-page/hero-section/GetStarted.tsx
@@ -1,11 +1,13 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import React from "react";
+const BIDPACK_APP_URL = "https://bidpackapp.com/";
 
 export default function GetStarted() {
   return (
     <Link
-      href={"/register"}
+      href={BIDPACK_APP_URL}
+      target="_blank"
       className={cn(
         "motion-delay-160 bg-[#5473C1] sm:text-md hover:opacity-80 transition-all px-[24px] py-[10px] font-semibold rounded-[12px] "
       )}
